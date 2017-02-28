65b1bcfe136435b026c38ea25cac344fe3b66b51
Boyi Zhang <bz11252@uga.edu>
2017-02-28 17:47:42 -0500

    working density fitted cepa0

--- CEPA0/df_cepa0.py
+++ CEPA0/df_cepa0.py
@@ -2,6 +2,7 @@ import psi4
 import numpy as np 
 from uhf import UHF
 import scipy.linalg as la
+import time
 
 class CEPA0:
 
@@ -12,6 +13,7 @@ class CEPA0:
         self.ntot = uhf.ntot
         self.nvir = self.ntot - self.nocc
         self.max_iter = uhf.cepa0_max_iter
+        self.den_fit = uhf.cepa0_df
         df = uhf.df_basis
         basis = uhf.basis
         mints = psi4.core.MintsHelper(basis) 
@@ -28,26 +30,24 @@ class CEPA0:
         pqP = spin_block_tei_df(pqP)
         pqP = np.squeeze(pqP)
         self.b_pqP = np.einsum('pqP,QP->pqQ',pqP,self.J_prime)
-        #self.E = 0.0 
+        self.E = 0.0 
     def get_energy(self): 
-        C, gao, nocc, nvir, e= self.C, self.gao, self.nocc, self.nvir, self.e
+        time_0 = time.time()
+        C, gao, nocc, nvir, e,E= self.C, self.gao, self.nocc, self.nvir, self.e,self.E
         # transform integrals
         gmo = int_trans_2(gao,C,C,C,C) 
-        E_old = 0.0
+        E_old = self.E
         t_old = np.zeros((nocc,nocc,nvir,nvir))
         o = slice(None,nocc)
         v = slice(nocc,None)
         x = np.newaxis
         e_ijab = 1./(e[o,x,x,x]+e[x,o,x,x]-e[x,x,v,x]-e[x,x,x,v])
         for iteration in range(self.max_iter):
-            #print(np.shape(gmo[o,v,v,o]))
-            #print(np.shape(t_old))
             t4 = np.einsum('kbcj,ikac -> ijab', gmo[o,v,v,o],t_old)
             t = (gmo[o,o,v,v]+(1/2)*np.einsum('abcd,ijcd->ijab',gmo[v,v,v,v],t_old)+(1/2)*np.einsum('klij,klab->ijab',gmo[o,o,o,o],t_old)+
                 t4.transpose((0,1,2,3))-t4.transpose((1,0,2,3))-t4.transpose((0,1,3,2))+t4.transpose((1,0,3,2)))
 
             t = t*e_ijab
-            #print(t)
 
             E_CEPA0 = (1/4)*np.einsum('ijab,ijab->',gmo[o,o,v,v],t)
             t_norm = np.linalg.norm(t-t_old)
@@ -57,60 +57,52 @@ class CEPA0:
                 break 
             E_old = E_CEPA0
             t_old = t
-        
+        time_1 = time.time()
         print('The UCEPA0 correlation energy is {:20.14f}'.format(E_CEPA0))
         print('The total UCEPA0 is {:20.14f}'.format(uhf.E_SCF - E_CEPA0))
-        #self.E = E
-        #return E 
+        print('UCEPA0 took: {:7.5} seconds'.format(time_1-time_0))
+        self.E = E_CEPA0
+        return E_CEPA0
                
-    def get_energy_df(self): 
+    def get_energy_df(self):
+        time_df_0 = time.time() 
         C, gao, nocc, nvir,ntot, e= self.C, self.gao, self.nocc, self.nvir, self.ntot,self.e
-        # transform integrals
         E_df_old = 0.0
         t_old = np.zeros((nocc,nocc,nvir,nvir))
         t2 = np.zeros((nocc,nocc,nvir,nvir))
         o = slice(None,nocc)
         v = slice(nocc,None)
         x = np.newaxis
-#        print(C[:,v])
-#        print(gmo1)
+        gmo_2 = np.zeros((nvir,nvir))
         gmo2 = np.zeros((nvir,nvir))
         gmo1 = int_trans_2(gao,C[:,v],C[:,v],C[:,o],C[:,o])
         gmo3 = int_trans_2(gao,C[:,o],C[:,o],C[:,o],C[:,o])
         gmo4 = int_trans_2(gao,C[:,o],C[:,v],C[:,v],C[:,o])
         gmo_caP = int_trans_df(self.b_pqP,C[:,v]) 
         e_ijab = 1./(e[o,x,x,x]+e[x,o,x,x]-e[x,x,v,x]-e[x,x,x,v])
- #       print(np.shape(gmo_caP))
+        
         for iteration in range(self.max_iter):
-            for c in range(nvir):
-                for d in range(c,nvir):
-                    gmo2 = np.einsum('aP,bP->ab',gmo_caP[c,:,:],gmo_caP[d,:,:])
-                    print(np.shape(gmo2))
-                    t2 = np.einsum('ab,ijcd->ijab',gmo2-gmo2.T,t_old)
-            #print(np.shape(t2))
-  #          print(np.shape(gmo4))
-   #         print(np.shape(t_old))
+            for a in range(nvir):
+                for b in range(nvir):
+                   gmo2 = np.einsum('cP,dP->cd',gmo_caP[a],gmo_caP[b])
+                   t2[:,:,a,b] = np.einsum('cd,ijcd->ij',(gmo2-gmo2.T),t_old)
             t4 = np.einsum('kbcj,ikac -> ijab', gmo4,t_old)
-            t = (gmo1+(1/2)*t2+(1/2)*np.einsum('klij,klab->ijab',gmo3,t_old)+
-                t4.transpose((0,1,2,3))-t4.transpose((1,0,2,3))-t4.transpose((0,1,3,2))+t4.transpose((1,0,3,2)))
+            t = e_ijab*(gmo1 + (1/2)*t2+(1/2)*np.einsum('klij,klab->ijab',gmo3,t_old)+
+                t4-t4.transpose((1,0,2,3))-t4.transpose((0,1,3,2))+t4.transpose((1,0,3,2)))
 
-            t = t*e_ijab
-            #print(t)
             E_CEPA0_df = (1/4)*np.einsum('ijab,ijab->',gmo1,t)
             t_norm = np.linalg.norm(t-t_old)
-            print('DF-UCEPA0 iteration {:3d}: energy {:20.14f} dE {:2.5E} t_norm {:2.5E}'.format(iteration, E_CEPA0_df,(E_CEPA0_df-E_df_old),t_norm))
+            #print('DF-UCEPA0 iteration {:3d}: energy {:20.14f} dE {:2.5E} t_norm {:2.5E}'.format(iteration, E_CEPA0_df,(E_CEPA0_df-E_df_old),t_norm))
 
             if (abs(E_CEPA0_df - E_df_old))<1.e-10 and t_norm < 1.e-10:
                 break 
             E_df_old = E_CEPA0_df
             t_old = t
-            t2 = np.zeros((nocc,nocc,nvir,nvir))
-        
+        time_df_1 = time.time()
         print('The DF-UCEPA0 correlation energy is {:20.14f}'.format(E_CEPA0_df))
         print('The total DF-UCEPA0 is {:20.14f}'.format(uhf.E_SCF - E_CEPA0_df))
-        #self.E = E
-        #return E 
-
+        print('DF-UCEPA0 error: {:20.14f}'.format(self.E - E_CEPA0_df))
+        print('DF-UCEPA0 took: {:7.5} seconds'.format(time_df_1-time_df_0))
 def spin_block_tei(gao):
     I = np.eye(2)
     gao = np.kron(I, gao)
@@ -139,13 +131,8 @@ if __name__ == '__main__':
     uhf = UHF('Options.ini')
     uhf.get_energy()
     cepa0 = CEPA0(uhf)
-    #cepa0.get_energy()
-    cepa0.get_energy_df()
-    psi4.set_options({'basis':'cc-pvdz',
-                        'scf_type': 'pk',
-                        'MP2_type' : 'conv',
-                        'puream' : False,
-                        'reference': 'uhf',
-                        'guess' : 'core',
-                        'e_convergence' : 1e-10})
-    #psi4.energy('mp2')
+    if cepa0.den_fit==1: 
+        cepa0.get_energy()
+        cepa0.get_energy_df()
+    else:
+        cepa0.get_energy()
